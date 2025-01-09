69399b3e21734d42d9d002ee6d19daef98094795
ashwani hada <ashwanihada13@gmail.com>
2025-01-09 11:36:25 +0530

    Project Done

--- index.php
+++ index.php
@@ -1,14 +1,20 @@
-<?php
-//Import PHPMailer classes into the global namespace
-//These must be at the top of your script, not inside a function
-use PHPMailer\PHPMailer\PHPMailer;
-use PHPMailer\PHPMailer\SMTP;
-use PHPMailer\PHPMailer\Exception;
+
+<?php 
 
 //Load Composer's autoloader
 require 'PHPMailer/Exception.php';
 require 'PHPMailer/PHPMailer.php';
 require 'PHPMailer/SMTP.php';
+$email='';
+$checkmail=false;
+
+if(isset($_POST['send'])){
+    $email=$_POST['email'];
+}
+
+use PHPMailer\PHPMailer\PHPMailer;
+use PHPMailer\PHPMailer\SMTP;
+use PHPMailer\PHPMailer\Exception;
 
 
 //Create an instance; passing `true` enables exceptions
@@ -16,7 +22,6 @@ $mail = new PHPMailer(true);
 
 try {
     //Server settings
-    $mail->SMTPDebug = SMTP::DEBUG_SERVER;                      //Enable verbose debug output
     $mail->isSMTP();                                            //Send using SMTP
     $mail->Host       = 'Smtp.gmail.com';                     //Set the SMTP server to send through
     $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
@@ -27,7 +32,7 @@ try {
 
     //Recipients
     $mail->setFrom('ashwanihada00@gmail.com', 'Ashion Website');
-    $mail->addAddress('mukulporwal05@gmail.com', 'Mukul Porwal');     //Add a recipient
+    $mail->addAddress($email);     //Add a recipient
  
 
    
@@ -35,11 +40,51 @@ try {
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
     $mail->Subject = 'Mail Someone Using Php';
-    $mail->Body    = 'Hi , I am Ash ... Thank You For Visiting MyWeb';
+    $mail->Body    = 'Hi , Thank You For Visiting Ashion . You Will Get Updates From Now';
     
 
     $mail->send();
-    echo 'Message has been sent';
+    $checkmail=true;
 } catch (Exception $e) {
-    echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
+    // echo "Message could not be sent. Mailer Error: {$mail->ErrorInfo}";
 }
+
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Email Using Php</title>
+    <link rel="stylesheet" href="style.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-QWTKZyjpPEjISv5WaRU9OFeRpok6YctnYmDr5pNlyT2bRjXh0JMhjY6hW+ALEwIH" crossorigin="anonymous">
+
+</head>
+
+<body >
+<?php 
+    if($checkmail){
+        echo'
+        <div class="alert alert-success" role="alert">
+        Congratulations .. ❤️ You Successfuly Subscribed Our NewsLetter.
+        </div>
+        ';
+    }
+?>
+<div class="card">
+    <span class="card__title">Subscribe</span>
+    <p class="card__content">Get fresh notification  About our Website to your inbox every week.
+    </p>
+    <div class="card__form">
+        <form action="index.php" method="post">
+        <input name="email" placeholder="Your Email" type="text">
+        <button class="sign-up" name="send" type="submit"> Sign up</button>
+        </form>
+    </div>
+   
+</div>
+
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
+
+</body>
+</html>
