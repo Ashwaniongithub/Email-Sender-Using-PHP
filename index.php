cd93e99e322d2d085829c80f557e63e301a4b461
ashwani hada <ashwanihada13@gmail.com>
2025-01-09 11:47:36 +0530

    Project Done

--- index.php
+++ index.php
@@ -39,7 +39,7 @@ try {
 
     //Content
     $mail->isHTML(true);                                  //Set email format to HTML
-    $mail->Subject = 'Mail Someone Using Php';
+    $mail->Subject = 'Ashion Newsletter ';
     $mail->Body    = 'Hi , Thank You For Visiting Ashion . You Will Get Updates From Now';
     
 
@@ -50,6 +50,9 @@ try {
 }
 
 ?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -62,26 +65,30 @@ try {
 </head>
 
 <body >
+<div class=" mt-5 d-flex justify-content-center" >
 <?php 
     if($checkmail){
         echo'
         <div class="alert alert-success" role="alert">
-        Congratulations .. ❤️ You Successfuly Subscribed Our NewsLetter.
+        Congratulations .. ❤️ You Successfully Subscribed Our NewsLetter.
         </div>
         ';
     }
 ?>
-<div class="card">
+</div>
+<div  class="d-flex align-items-center justify-content-center mt-5">
+<div class="card" style="width: 20%; padding:30px;">
     <span class="card__title">Subscribe</span>
     <p class="card__content">Get fresh notification  About our Website to your inbox every week.
     </p>
     <div class="card__form">
         <form action="index.php" method="post">
         <input name="email" placeholder="Your Email" type="text">
-        <button class="sign-up" name="send" type="submit"> Sign up</button>
+        <button class="sign-up mt-4" name="send" type="submit"> Sign up</button>
         </form>
     </div>
    
+</div>
 </div>
 
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-YvpcrYf0tY3lHB60NNkmXc5s9fDVZLESaAA55NDzOxhy9GkcIdslK1eN7N6jIeHz" crossorigin="anonymous"></script>
